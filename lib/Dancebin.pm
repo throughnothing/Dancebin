79da10c436a88b1b8b38cf5a0cc25128b17f0c2d
Naveed Massjouni <naveedm9@gmail.com>
2012-05-08 03:17:38 -0400

    Adding links to each line via html anchors.

--- lib/Dancebin.pm
+++ lib/Dancebin.pm
@@ -7,8 +7,10 @@ use DateTime;
 use Dancebin::Pygments qw( highlight );
 use Data::UUID;
 
-
 # ABSTRACT: Simple. Pastebin.
+
+# Create the db if this is the first time app is ever run.
+eval { schema->deploy };
 
 get '/' => sub { template 'index.tt' };
 
@@ -17,15 +19,19 @@ post '/' => sub {
     return send_error("No Code!", 400) unless $p->{code};
 
     ( my $id = Data::UUID->new->create_b64 ) =~ s/\W//g;
+
+    my $html = highlight(
+        code  => $p->{code},
+        lang  => $p->{lang},
+        title => $p->{title}
+    );
+    $html = _add_anchor_links($html);
+
     return redirect '/' . schema->resultset('Post')->create({
         id    => $id,
         title => $p->{title},
         code  => $p->{code},
-        html  => highlight(
-            code  => $p->{code},
-            lang  => $p->{lang},
-            title => $p->{title}
-        ),
+        html  => $html,
         language => $p->{lang},
     })->id;
 };
@@ -51,6 +57,25 @@ get '/:id/raw' => sub {
     content_type 'text/plain';
     return vars->{post}->code;
 };
+
+sub _add_anchor_links {
+    my ($html) = @_;
+    my @html_with_anchors;
+    for (split /\n/, $html) {
+        if (m{<div class="linenodiv"><pre>\s*1$} .. m{</pre>}) {
+            if (m{</pre>}) { # the last line is a special case
+                s{^(\d+)</pre>.*}
+                 {<a name="l$1"></a><a href="#l$1">$1</a></pre>};
+            } else {
+                s{(\d+)$}{<a name="l$1"></a><a href="#l$1">$1</a>};
+            }
+            push @html_with_anchors, $_;
+        }
+    }
+    my $html_with_anchors = join "\n", @html_with_anchors;
+    $html =~ s{^.+<div class="linenodiv"><pre>.+?</pre>}{$html_with_anchors}s;
+    return $html;
+}
 
 1;
 
